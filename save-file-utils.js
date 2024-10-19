e5f1d5da7f3821f01a2e15a2430a24dfba17272d
Sam Sarette <samuelsarette@linux.com>
2024-10-18 20:25:35 -0400

    prefer straight download to prompting

--- save-file-utils.js
+++ save-file-utils.js
@@ -25,7 +25,7 @@
  * @param {FileMimeTypeOption} mimeOptions      MIME type info options
  * @param {StartPathType}  [startIn]            Where to start the save-as dialog from
  */
-export async function saveBlobAs(filename, blobData, mimeOptions, startIn = 'downloads') {
+export async function saveBlobAs(filename, blobData, mimeOptions, startIn = 'downloads', promptIfFallback = false) {
     /**
      * @type {SaveFilePickerOptions}
      */
@@ -51,15 +51,16 @@ export async function saveBlobAs(filename, blobData, mimeOptions, startIn = 'dow
 
         console.warn('file picker method failed, falling back to link element with download attribute. ', ex)
 
-        try {
-            // Use the browser prompt for name selection
-            filename = prompt("Enter file name for download", filename)
-            if (typeof(filename) !== 'string')
-                return; // user chose to cancel
-        } catch (ex) {
+        if (promptIfFallback)
+            try {
+                // Use the browser prompt for name selection
+                filename = prompt("Enter file name for download", filename)
+                if (typeof(filename) !== 'string')
+                    return; // user chose to cancel
+            } catch (ex) {
 
-            console.warn('Prompt failed, using default filename. ', ex)
-        }
+                console.warn('Prompt failed, using default filename. ', ex)
+            }
 
         // Classic method, only gives users a choice if they tell their browser to for all downloads
         const link = document.createElement("a");
