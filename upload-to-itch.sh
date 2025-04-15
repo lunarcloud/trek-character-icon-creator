2fb633aa0c2d0196de84965cc2a4d9a07f38c5c7
Sam Sarette <samuelsarette@linux.com>
2025-04-15 11:55:13 -0400

    fixes to new stuff

--- upload-to-itch.sh
+++ upload-to-itch.sh
@@ -12,6 +12,7 @@ cp -r fonts $EXPORT
 cp -r cetaceous $EXPORT
 cp -r humanoid $EXPORT
 cp -r exocomp $EXPORT
+cp -r medusan $EXPORT
 
 echo Uploading to Itch...
 butler -v push $EXPORT "samsarette/simple-trekkie-character-creator:web"
